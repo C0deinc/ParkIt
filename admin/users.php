1eb49c98a239db83cb8896b7f5ba0cd944c2865e
Kathleen Kate Santiago <145891887+kathleenkate16@users.noreply.github.com>
2024-02-11 09:26:42 +0800

    Update users.php

--- admin/users.php
+++ admin/users.php
@@ -1,35 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Users</title>
+<?php
+    $title = 'Users';
+    $users_page = 'active';
+    require_once('./include/head.php');
+?>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link href="./vendor/font-awesome-4.7.0/css/font-awesome.min.css">
-
-    <!-- Chart.js -->
-    <script defer src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body style="overflow: hidden;">
+<body>
+    <!-- Page Content -->
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Sidebar -->
+            <?php
+            require_once('./include/sidebar.php');
+            ?>
     <style>
         tr[data-href] {
             cursor: pointer;
         }
     </style>
-    <!-- Navigation Bar -->
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #06283D;">
-        <a class="navbar-brand ms-4" href="#">
-            <img src="logo1.png" alt="logo" width="60" height="30">
-        </a>
-    </nav>
 
     <!-- Page Content -->
     <div class="container-fluid">
