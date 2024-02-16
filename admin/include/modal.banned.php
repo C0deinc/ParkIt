8260daab27e64af55eaf01aa45e48fc6d7e64efa
InoferioCharls <CodeIncwmsu@gmail.com>
2024-02-16 18:39:42 +0800

    new files
    created by me if naa mali pa coreect na lng pls

---
+++ admin/include/modal.banned.php
@@ -0,0 +1,57 @@
+<div class="modal fade" id="banned" data-bs-backdrop="static" tabindex="-1" aria-labelledby="bannedaccount" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header justify-content-center flex-column">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <h1 class="modal-title fs-5" id="bannedaccount">Warning Account</h1>
+      </div>
+      <form action="post" method="post">
+        <div class="modal-body">
+          <form action="post" method="post">
+            <div class="row mb-3">
+              <label for="user" class="col-sm-2 col-form-label">Name:</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="user" required placeholder="Enter your Username..." style="width: 100%;">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="account" class="col-sm-2 col-form-label">Date:</label>
+              <div class="col-sm-10">
+                <input type="email" class="form-control" id="account" required placeholder="Enter your email..." style="width: 100%;">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="pass" class="col-sm-2 col-form-label">Contact:</label>
+              <div class="col-sm-10">
+                <input type="password" class="form-control" id="pass" required placeholder="Enter your password..." style="width: 100%;">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="dropdown d-flex align-items-center">
+                <label for="warning">Warning Violation: </label>
+                <select name="warning" id="warninglevel" class="form-select" style="width: 40%; margin-left: 1rem;">
+                  <option value="Warning Level 1">Warning Level 1</option>
+                  <option value="Warning Level 2">Warning Level 2</option>
+                  <option value="Warning Level 3">Warning Level 3</option>
+                </select>
+              </div>
+            </div>
+            <div class="row mb-3 d-flex flex-direction-column">
+              <label for="descriptiom" class="col-form-label">Reason for Banning: </label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="account" required placeholder="Enter your statement..." style="width: 121%; height: 13vh;">
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="containline d-flex justify-content-center" style="width: 100%;">
+          <div class="line" style="border-bottom: 1px solid black; width: 90%;"></div>
+        </div>
+        <div class="modal-footer d-flex justify-content-center">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+          <button type="button" class="btn btn-primary">Okay</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
---
+++ admin/warning.account.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Warning Accounts Page</title>
+
+    <?php
+    require_once('./include/head.php');
+    ?>
+    <!-- Chart.js -->
+    <script defer src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+</head>
+
+<body style="overflow: hidden;">
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #06283D;">
+        <a class="navbar-brand ms-4" href="#">
+            <img src="logo1.png" alt="logo" width="60" height="30">
+        </a>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Sidebar -->
+            <?php
+            require_once('./include/sidebar.php');
+            require_once('./include/modal.warning.php')
+            ?>
+
+            <!-- Main Content -->
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-6" style="padding-top: 5px;">
+                <div class="d-flex ms-5 mt-4">
+                    <h4 class="h3 m">Warning Accounts</h1>
+                </div>
+                <div class="box_main d-flex justify-content-around" id="MyButtons" class="d-flex mb-md-2 mb-lg-0 col-12 col-md-auto">
+                    ContentPDF
+                    <div class="search-keyword d-flex">
+                        <div class="input-group pe-4">
+                            <input type="text" name="keyword" id="keyword" placeholder="Search..." class="form-control">
+                            <button class="btn btn-outline-secondary brand-bg-color" type="button" style="border: none;">
+                                <i class="fa fa-search color-white" aria-hidden="true"></i>
+                            </button>
+                        </div>
+                        <div id="MyButtons" class="d-flex mb-md-2 mb-lg-0 col-12 col-md-auto"></div>
+                        <div class="form-group col-2 col-sm-auto flex-sm-grow-1 flex-lg-grow-0 ms-lg-auto">
+                            <select name="staff-role" id="staff-role" class="form-select me-md-2">
+                                <option value="">Sort by</option>
+
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="table-box">
+                    <div class="table-row-header">
+                        <div class="table-cell">
+                            <h5></h5>
+                        </div>
+                        <div class="table-cell">
+                            <h5>Name</h5>
+                        </div>
+                        <div class="table-cell">
+                            <h5>Account</h5>
+                        </div>
+                        <div class="table-cell">
+                            <h5>Date Applied</h5>
+                        </div>
+                        <div class="table-cell">
+                            <h5>More</h5>
+                        </div>
+                    </div>
+                    <div class="table-row">
+                        <div class="table-cell">
+                            <img class="rounded-circle" style="width: 50px; height: 50px;" src="../admin/images/jau.jpg" alt="">
+                        </div>
+                        <div class="table-cell">
+                            <p>Mohammad Jauhari Sali S.</p>
+                        </div>
+                        <div class="table-cell">
+                            <p>0012457</p>
+                        </div>
+                        <div class="table-cell">
+                            <p>01/24/24</p>
+                        </div>
+                        <div class="table-cell">
+                            <button class="butm" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <p><i class="fa fa-ellipsis-h" aria-hidden="true"></i></p>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- Overview Content Goes Here -->
+
+
+            </main>
+        </div>
+    </div>
+
+    <!-- Include Bootstrap JS and jQuery -->
+    <?php
+    require_once('../admin/scripts/script.php');
+
+    ?>
+
+</body>
+
+</html>
