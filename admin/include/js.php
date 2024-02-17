f5314bf8e87ae1e978890683127b61900d79b4ce
Kathleen Kate Santiago <145891887+kathleenkate16@users.noreply.github.com>
2024-02-17 15:29:19 +0800

    add

---
+++ admin/include/js.php
@@ -0,0 +1,9 @@
+<script src="../vendor/bootstrap-5.0.2/js/bootstrap.bundle.min.js"></script>
+<script src="../vendor/jquery-3.7.1/jquery.min.js"></script>
+<script src="../vendor/dataTable-1.13.6/datatables.min.js"></script>
+<script src="../vendor/pdfmake-0.2.7/pdfmake.min.js"></script>
+<script src="../vendor/pdfmake-0.2.7/vfs_fonts.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.6/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.6/js/jquery.dataTables.js"></script>
