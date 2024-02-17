93329f41bad868c453a6f330deeb8a264c71320a
Kathleen Kate Santiago <145891887+kathleenkate16@users.noreply.github.com>
2024-02-17 14:53:33 +0800

    update

--- admin/reports.php
+++ admin/reports.php
@@ -6,9 +6,7 @@
     $report_page = 'active';
     require_once('./include/head.php');
 ?>
-
 <body>
-
     <!-- Page Content -->
     <div class="container-fluid">
         <div class="row">
@@ -16,88 +14,92 @@
             <?php
             require_once('./include/sidebar.php');
             ?>
-    <!-- Main Content -->
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-6" style="padding-top: 30px;">
-                <div class="row justify-content-center">
-                    <div class="col-md-4">
-                        <a href="uf&rs.html" class="card-link">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">
-                                        USER FEEDBACK AND REVIEWS
-                                    </h5>
-                                    <p class="card-text text-center" style="font-size: 30px;">100</p>
-                                </div>
+                    <!-- Main Content -->
+                    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-6" >
+                        <div class="row d-flex justify-content-center align-items-center pt-3">
+                            <div class="col-md-4">
+                                <a href="uf&rs.php" class="card-link">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h5 class="card-title text-center">
+                                                USER FEEDBACK AND REVIEWS
+                                            </h5>
+                                            <p class="card-text text-center" style="font-size: 30px;">100</p>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
-                        </a>
-                    </div>
-                    <div class="col-md-4">
-                        <a href="#" class="card-link"></a>
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">
-                                        WARNING
-                                    </h5>
-                                    <p class="card-text text-center" style="font-size: 30px;">500</p>
-                                </div>
+                            <div class="col-md-4">
+                                <a href="#" class="card-link"></a>
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h5 class="card-title text-center">
+                                                WARNING
+                                            </h5>
+                                            <p class="card-text text-center" style="font-size: 30px;">500</p>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
-                        </a>
-                    </div>
-                </div>
-                <!-- Line Graph and Bar Graph Side by Side -->
-                <div class="container mt-4">
-                    <div class="row justify-content-center">
-                        <div class="col-md-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <a href="ts.html" class="card-link">
-                                                <div class="card">
-                                                    <div class="card-body">
-                                                        <h6 class="card-title">TRANSACTION</h6>
-                                                        <canvas id="transaction" height="200"></canvas>
+                        </div>
+                        <!-- Line Graph and Bar Graph Side by Side -->
+                        <div class="container mt-4">
+                            <div class="row justify-content-center">
+                                <div class="col-md-12">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <a href="ts.php" class="card-link">
+                                                        <div class="card">
+                                                            <div class="card-body">
+                                                                <h6 class="card-title">TRANSACTION</h6>
+                                                                <canvas id="transaction" height="200"></canvas>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="card">
+                                                        <div class="card-body">
+                                                            <h6 class="card-title">INCIDENTS</h6>
+                                                            <canvas id="incident" height="200"></canvas>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </a>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="card">
-                                                <div class="card-body">
-                                                    <h6 class="card-title">INCIDENTS</h6>
-                                                    <canvas id="incident" height="200"></canvas>
-                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                        <div class="container mt-4">
+                            <div class="row justify-content-center">
+                                <div class="col-md-12">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md-5">
+                                                    <div style="position: relative;">
+                                                        <canvas id="areaScalingChart" height="200"></canvas>
+                                                        <div style="position: absolute; top: 50%; right: -500px; transform: translateY(-50%); text-align: right;">
+                                                            <h5 class="card-title" style="font-size: 50px;"> AREA SCALING</h5>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>            
+                    </main>
                 </div>
-                <div class="container mt-4">
-                    <div class="row justify-content-center">
-                        <div class="col-md-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-5">
-                                            <div style="position: relative;">
-                                                <canvas id="areaScalingChart" height="200"></canvas>
-                                                <div style="position: absolute; top: 50%; right: -500px; transform: translateY(-50%); text-align: right;">
-                                                    <h5 class="card-title" style="font-size: 50px;"> AREA SCALING</h5>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>            
-            </main>
-        </div>
-    </div>
+            </div>
+
+    <?php
+        require_once('./include/js.php')
+    ?>
 
     <!-- Include Bootstrap JS and jQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
