1b5d27b04d50b863b1f0c724b00877a1ede79d57
Kathleen Kate Santiago <145891887+kathleenkate16@users.noreply.github.com>
2024-02-17 15:01:46 +0800

    update

--- admin/wallet_user.php
+++ admin/wallet_user.php
@@ -83,5 +83,15 @@
             </div>             
         </main>   
     </div>
+
+    <?php
+        require_once('./include/js.php')
+    ?>
+
+    <!-- Include Bootstrap JS and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    
 </body>
 </html>
