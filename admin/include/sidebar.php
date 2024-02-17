ec47aaf114273132865275434b6880dadef32bde
Kathleen Kate Santiago <145891887+kathleenkate16@users.noreply.github.com>
2024-02-17 16:10:17 +0800

    update

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -2,6 +2,20 @@
         <a class="navbar-brand ms-4" href="#">
             <img src="/admin/images/logo1.png" alt="" style="width: 60px; height: 30px;">
         </a>
+        <div class="navbar-collapse offcanvas-collapse ms-auto">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle color-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+                    <img src="/admin/images/photo.png" class="rounded-circle me-2" alt="User Image" width="30px" height="30px" style="position: relative; top: -2px;"> <!-- Adjust position as needed -->
+                    Charl's
+                </a>
+                <ul class="dropdown-menu dropdown-profile mt-0" aria-labelledby="navbarDropdown" style="margin-left: -40px;">
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li><a class="dropdown-item" href="../logout.php">Logout</a></li>
+                </ul>
+                </li>
+            </ul>
+        </div>
     </nav>
 
     <!-- Page Content -->
@@ -12,31 +26,31 @@
                 <div class="position-sticky">
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <a class="nav-link <?= $index_page ?>"  aria-current="page" href="index.php">
+                            <a class="nav-link <?= $index_page?>" aria-current="page" href="index.php">
                                 <img src="/admin/images/dashboard.png" alt="dashboard" width="20" height="20" class="mr-2">
                                 Dashboard
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link <?= $user_page ?>" href="users.php">
+                            <a class="nav-link <?= $users_page?>" aria-current="page" href="users.php">
                                 <img src="/admin/images/user.png" alt="users" width="20" height="20" class="mr-2">
                                 Users
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link <?= $report_page?>" href="reports.php">
+                            <a class="nav-link <?= $report_page?>" aria-current="page" href="reports.php">
                                 <img src="/admin/images/report.png" alt="report" width="20" height="20" class="mr-2">
                                 Reports
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link <?= $staff_page?>" href="staff.php">
+                            <a class="nav-link <?= $staff_page?>" aria-current="page" href="staff.php">
                                 <img src="/admin/images/staff.png" alt="staff" width="20" height="20" class="mr-2">
                                 Staff
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link <?= $setting_page?>" href="setting.php">
+                            <a class="nav-link <?= $setting_page?>" aria-current="page" href="setting.php">
                                 <img src="/admin/images/setting.png" alt="setting" width="20" height="20" class="mr-2">
                                 Setting
                             </a>
@@ -45,13 +59,6 @@
                             <a class="nav-link <?= $wallet_page?>" href="wallet.php">
                                 <img src="/admin/images/wallet.png" alt="wallet" width="20" height="20" class="mr-2">
                                 Wallet
-                            </a>
-                        </li>
-                        <div class="dropdown-divider"></div>
-                        <li class="nav-item">
-                            <a class="nav-link" href="login.php">
-                                <img src="/admin/images/logout.png" alt="logout" width="20" height="20" class="mr-2">
-                                Log Out
                             </a>
                         </li>
                     </ul>
