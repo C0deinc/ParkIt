ab2dcbe3606cbf1fc72edbe5353a78897745bdfe
Kathleen Kate Santiago <145891887+kathleenkate16@users.noreply.github.com>
2024-02-17 14:55:00 +0800

    new update

---
+++ admin/setting.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+    $title = 'Setting';
+    $setting_page = 'active';
+    require_once('./include/head.php');
+?>
+
+<body>
+
+    <!-- Page Content -->
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Sidebar -->
+            <?php
+            require_once('./include/sidebar.php');
+            ?>
+
+    <!-- Include Bootstrap JS and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> 
