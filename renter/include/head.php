2d507dc717a0775dcfa719f6eef7907ba8709d4d
InoferioCharls <CodeIncwmsu@gmail.com>
2024-03-12 22:55:48 +0800

    new renter not finished
    NEW RENTER 0cha

---
+++ renter/include/head.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title ?></title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="./vendor/dataTable-1.13.6/datatables.min.css" rel="stylesheet">
+
+    <!-- Chart.js -->
+    <script defer src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/admin/css/style.css">
+</head>
---
+++ renter/include/pay.all.modal.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="payall" data-bs-backdrop="static" tabindex="-1" aria-labelledby="payallmodal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content p-4">
+            <div class="modal-header justify-content-center flex-column">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h1 class="modal-title fs-5" id="payallmodal">Pay All</h1>
+            </div>
+            <div class="Paying section">
+                <div class="table-box mt-4">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
