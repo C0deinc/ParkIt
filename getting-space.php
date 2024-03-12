26719f7652bf20030a0e3cb46eedad40e5cce123
harolhette <maryharolhetterom@gmail.com>
2024-03-13 00:15:43 +0800

    yawa

--- getting-space.php
+++ getting-space.php
@@ -208,6 +208,8 @@
                                                 </div>
                                             </div>
 
+                                            
+
                                             <div>
                                                 <div class="row d-flex flex-column p-3">
                                                     <div class="col-sm-12 col-lg-6">
@@ -251,26 +253,7 @@
                                         </div>
 
                                         <div class="modal-footer">
-                                            <button type="button" class="btn" data-bs-dismiss="modal" style="background-color: #1363DF; color: #DFF6FF">Next <i class="fa-solid fa-arrow-right ms-1"></i></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="modal fade" id="newModal" tabindex="-1" aria-labelledby="newModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="newModalLabel">C1 Details</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        
-                                        <div class="modal-body">
-                                            <!-- Content for the third modal goes here -->
-                                        </div>
-
-                                        <div class="modal-footer">
-                                            <!-- Add any footer buttons or controls here -->
+                                            <button type="button" class="btn" data-bs-dismiss="modal" style="background-color: #1363DF; color: #DFF6FF">Done</button>
                                         </div>
                                     </div>
                                 </div>
@@ -384,13 +367,10 @@
                     }
 
                     $('#modalC1').on('hidden.bs.modal', function () {
-                        // Close the modal completely when the first modal is hidden
                         $('#modalC1').modal('hide');
                     });
 
-                    // Add event listener to the "Next" button in the first modal
                     $('#modalC1').on('click', '.modal-footer button', function() {
-                        // Show the second modal when the "Next" button is clicked
                         $('#newModal').modal('show');
                     });
                 });
@@ -401,5 +381,6 @@
     <script src="https://kit.fontawesome.com/a3bf0b829f.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
 </body>
 </html>
